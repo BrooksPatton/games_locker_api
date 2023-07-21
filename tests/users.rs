3d6fbf78dad91176c6fec9032f7c904274f3c456
Brookzerker <brooks@brookzerker.com>
2023-07-20 20:01:54 -0600

    setting up test stubs

---
+++ tests/users.rs
@@ -0,0 +1,23 @@
+#[tokio::test]
+#[ignore = "tbd"]
+async fn should_create_user() {}
+
+#[tokio::test]
+#[ignore = "tbd"]
+async fn should_log_in() {}
+
+#[tokio::test]
+#[ignore = "tbd"]
+async fn should_log_out() {}
+
+#[tokio::test]
+#[ignore = "tbd"]
+async fn should_delete_user() {}
+
+#[tokio::test]
+#[ignore = "tbd"]
+async fn should_access_protected_route() {}
+
+#[tokio::test]
+#[ignore = "tbd"]
+async fn should_not_access_protected_route_without_token() {}
